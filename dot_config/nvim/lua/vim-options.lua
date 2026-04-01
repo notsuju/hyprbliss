2270d8ce4366cd17248c14883d69fcf5eeef697f
notsuju <notsuju@proton.me>
2026-04-01 22:16:15 +0530

    Zenbook Duo Rice

---
+++ dot_config/nvim/lua/vim-options.lua
@@ -0,0 +1,13 @@
+-- vim.opt.number = true
+vim.g.mapleader = " "
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.updatetime = 100
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
