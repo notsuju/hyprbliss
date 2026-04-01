a2be2af3c8bd2487d47b914f77713d237d352b41
notsuju <notsuju@proton.me>
2026-04-02 01:08:30 +0530

    Finalized installion script

--- scripts/install_programs.sh
+++ scripts/install_programs.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-echo "Installing Core Setup..."
+echo "Installing Core Setup and Plugins..."
 sudo pacman -Syu --noconfirm
-sudo pacman -S hyprland waybar kitty fuzzel mako btop cava neovim yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick mpv --noconfirm
-yay -S swappy-git waypaper uwufetch ani-cli zen-browser-bin --noconfirm
+sudo pacman -S hyprland waybar kitty fuzzel mako btop cava neovim yazi \
+ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick mpv \
+hypridle hyprlock awww nwg-displays nwg-look hyprcursor \
+pavucontrol blueman obs-studio bluez bluez-utils --noconfirm
+yay -S swappy-git waypaper uwufetch ani-cli zen-browser-bin localsend-bin --noconfirm
+hyprpm update
+hyprpm add https://github.com/hyprwm/hyprland-plugins
+hyprpm add https://github.com/VirtCode/hypr-dynamic-cursors
+hyprpm enable hyprexpo
+hyprpm reload
 echo "Done!"
 
 echo "Installing Developer Tools..."
