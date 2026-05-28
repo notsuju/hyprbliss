5f9a4707e26833962e8c65feb63d7b30ce9aa6d3
Suju <131643792+notsuju@users.noreply.github.com>
2026-05-28 16:26:30 +0530

    Delete dot_config directory

--- dot_config/scripts/executable_toggle-nightlight.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if pkill -x wlsunset; then
-    notify-send -t 1500 "Night Light" "Disabled ☀️"
-else
-    wlsunset -l 20.5 -L 78.9 &
-    notify-send -t 1500 "Night Light" "Enabled 🌙"
-fi
